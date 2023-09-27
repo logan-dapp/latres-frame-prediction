9f6ed598e5848317412a11663db3757cf418b5ba
logan-dapp <144955329+logan-dapp@users.noreply.github.com>
2023-09-26 20:08:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from dataset import HatmanDownloader, VideoSeparator
+from dataset import HatmanDownloader, VideoSeparator, make_square
+from latent import images_to_latents, latents_to_images, generate_dataset, check_initialize
 
+check_initialize(torch_device='cuda')
 downloader = HatmanDownloader()
 file = next(downloader)
+
 separator = VideoSeparator(file)
-separator.get_next_frames()
+image_paths = separator.get_next_frames(frames=2)
+for image_path in image_paths:
+    make_square(image_path, outpath=image_path)
+latents = images_to_latents(image_paths)
+X_dat, Y_dat = generate_dataset(latents)
+latents_to_images(X_dat+Y_dat, 'output_images')
